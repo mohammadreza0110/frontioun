4174ce0bf21da5bd4c5885bac05b45a224b85946
Mhmdrza <mohammadreza0216@gmail.com>
2023-04-02 03:41:29 +0430

    feat: trans for sidebar + stopProp + items divide

--- layouts/SideMenu.tsx
+++ layouts/SideMenu.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./Header/style";
 import { MdOutlineClose } from "react-icons/md";
+import MobileSearch from "@/components/SearchInput/MobileSearch";
+import DarkModeButton from "@/components/Swapers/DarkModeButton";
 
 export default function SideMenu(props: React.ComponentState) {
   const { toggleMobileMenu, setToggleMobileMenu } = props;
+
+  const [themeText, setThemeText] = useState();
+
+  useEffect(() => {
+    document.addEventListener("click", () => {
+      setToggleMobileMenu(false);
+    });
+  });
   return (
     <>
-      {toggleMobileMenu && (
-        <aside className="fixed inset-0 z-50 h-screen py-8 center w-72 bg-primary-light dark:bg-primary-dark">
+      <aside
+        className={classes.side_bar.wrapper(toggleMobileMenu)}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <section>
           <div className={classes.side_bar.frontioun_logo.wraper}>
             <span className={classes.side_bar.frontioun_logo.tags}>
               {"</ "}
@@ -17,13 +30,30 @@ export default function SideMenu(props: React.ComponentState) {
             </span>
             <span className={classes.side_bar.frontioun_logo.tags}>{">"}</span>
           </div>
-          <MdOutlineClose
-            onClick={() => setToggleMobileMenu(false)}
-            className="absolute cursor-pointer top-4 left-3"
-            size={22}
-          />
-        </aside>
-      )}
+
+          <ul className="grid grid-cols-1 divide-y divide-zinc-300 dark:divide-slate-400">
+            <li>
+              <MobileSearch />
+            </li>
+            <li className="flex items-center py-4 mt-8 gap-x-3">
+              <DarkModeButton />
+              <span>تم</span>
+            </li>
+            <li className="py-4 mt-8">
+              <DarkModeButton />
+            </li>
+            <li className="py-4 mt-8">
+              <DarkModeButton />
+            </li>
+          </ul>
+        </section>
+
+        <MdOutlineClose
+          onClick={() => setToggleMobileMenu(false)}
+          className="absolute cursor-pointer top-4 left-3"
+          size={22}
+        />
+      </aside>
     </>
   );
 }
