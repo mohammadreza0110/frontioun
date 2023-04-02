9e65d8124ab34e57cfbcd0215ad8e9d1d771bde2
Mhmdrza <mohammadreza0216@gmail.com>
2023-04-03 02:05:21 +0430

    create style.ts for index page

---
+++ assets/styles/pages/index.ts
@@ -0,0 +1,15 @@
+const classes = {
+  main: {
+    wrapper: "container px-10 mx-auto",
+    hero: {
+      wrapper: "py-8 center lg:center-center",
+      text: {
+        wrapper: "w-1/2",
+        title: "hero_text text-slate-700 dark:text-slate-200",
+        subTitle: "w-10/12 py-3 text-lg border-b-2 text-slate-500",
+      },
+    },
+  },
+};
+
+export default classes;
