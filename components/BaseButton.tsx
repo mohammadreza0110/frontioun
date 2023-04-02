e76e32e302451fdae55c1cf2548845f296515bae
Mhmdrza <mohammadreza0216@gmail.com>
2023-04-03 02:05:38 +0430

    feat: create base button

---
+++ components/BaseButton.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BaseButton() {
+  return <button className="w-5/12 h-16 mt-6 text-lg text-white bg-blue-700 border-none rounded-full hover:bg-blue-600 btn btn-info">بریم تو کارش!</button>;
+}
