3aa5a049bb4e79ff885ae799e099c18ac684a2f1
Mhmdrza <mohammadreza0216@gmail.com>
2023-04-12 01:29:02 +0430

    fix: base button add title & interface

--- components/BaseButton.tsx
+++ components/BaseButton.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import useWave from "use-wave";
+interface Props {
+  classes: string;
+  name: string | number;
+  title?: string;
+  children?: React.ReactNode;
+}
 
-export default function BaseButton({ classes, title, children }: any) {
+const BaseButton: React.FC<Props> = ({
+  classes,
+  name,
+  title,
+  children,
+}: Props) => {
   const wave = useWave({
     color: "#eee",
     easing: "ease-in",
   });
+
   return (
     <button
       ref={wave}
       className={`${classes} center-center gap-x-1.5 px-4 py-2 active:scale-90 !outline-none text-white cursor-pointer transition-all`}
+      title={title}
     >
-      {title}
+      {name}
       {children && <>{children}</>}
     </button>
   );
-}
+};
+
+export default BaseButton;
