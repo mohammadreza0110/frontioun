166a328ae164e39c811ee294bb0e72aad7f8d732
Mhmdrza <mohammadreza0216@gmail.com>
2023-04-12 01:30:45 +0430

    fix: syntax

--- components/GarrantyCard.tsx
+++ components/GarrantyCard.tsx
@@ -8,9 +8,9 @@ interface Props {
 
 export default function GarrantyCard({ children, title }: Props) {
   return (
-    <li className="flex flex-col items-center justify-center w-40 h-40 transition-all duration-200 hover:scale-105 hover:garranty_card bg-slate-200 hover:bg-white rounded-3xl">
+    <div className="flex flex-col items-center justify-center w-40 h-40 transition-all duration-200 hover:scale-105 hover:garranty_card bg-slate-200 hover:bg-white rounded-3xl">
       {children}
       <span>{title}</span>
-    </li>
+    </div>
   );
 }
