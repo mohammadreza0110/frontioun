a8f042acdac8e2037b427b7f280fb5ec22b104f0
Mhmdrza <mohammadreza0216@gmail.com>
2023-04-17 07:22:12 +0430

    fix: comment system english to persian

--- components/CommentSystem.tsx
+++ components/CommentSystem.tsx
@@ -30,20 +30,21 @@ export default function CommentSystem() {
               ارسال
             </button>
           </form>
+
           <article className="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
             <footer className="flex items-center justify-between mb-2">
               <div className="flex items-center">
-                <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                <p className="inline-flex items-center ml-3 text-sm text-gray-900 dark:text-white">
                   <img
-                    className="w-6 h-6 mr-2 rounded-full"
+                    className="w-6 h-6 ml-2 rounded-full"
                     src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
-                    alt="Michael Gough"
+                    alt="محمدرضا خورسند"
                   />
-                  Michael Gough
+                  محمدرضا خورسند
                 </p>
                 <p className="text-sm text-gray-600 dark:text-gray-400">
                   <time dateTime="2022-03-12" title="March 12th, 2022">
-                    Mar. 12, 2022
+                    8 اردیبهشت 1402
                   </time>
                 </p>
               </div>
@@ -78,7 +79,7 @@ export default function CommentSystem() {
                       href="#"
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
-                      Edit
+                      ویرایش
                     </a>
                   </li>
                   <li>
@@ -86,7 +87,7 @@ export default function CommentSystem() {
                       href="#"
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
-                      Remove
+                      حذف
                     </a>
                   </li>
                   <li>
@@ -94,17 +95,19 @@ export default function CommentSystem() {
                       href="#"
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
-                      Report
+                      گزارش
                     </a>
                   </li>
                 </ul>
               </div>
             </footer>
             <p className="text-gray-500 dark:text-gray-400">
-              Very straight-to-point article. Really worth time reading. Thank
-              you! But tools are just the instruments for the UX designers. The
-              knowledge of the design tools are as important as the creation of
-              the design strategy.
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+              استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+              در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
+              نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+              کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
+              جامعه و متخصصان را می طلبد،
             </p>
             <div className="flex items-center mt-4 space-x-4">
               <button
@@ -120,30 +123,31 @@ export default function CommentSystem() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                   ></path>
                 </svg>
-                Reply
+                پاسخ
               </button>
             </div>
           </article>
-          <article className="p-6 mb-6 ml-6 text-base bg-white rounded-lg lg:ml-12 dark:bg-gray-900">
+
+          <article className="p-6 mb-6 mr-6 text-base bg-white rounded-lg lg:ml-12 dark:bg-gray-900">
             <footer className="flex items-center justify-between mb-2">
               <div className="flex items-center">
-                <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                <p className="inline-flex items-center ml-3 text-sm text-gray-900 dark:text-white">
                   <img
-                    className="w-6 h-6 mr-2 rounded-full"
+                    className="w-6 h-6 ml-2 rounded-full"
                     src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                    alt="Jese Leos"
+                    alt="محمدرضا خورسند"
                   />
-                  Jese Leos
+                  محمدرضا خورسند
                 </p>
                 <p className="text-sm text-gray-600 dark:text-gray-400">
                   <time dateTime="2022-03-12" title="March 12th, 2022">
-                    Mar. 12, 2022
+                    28 فروردین 1402
                   </time>
                 </p>
               </div>
@@ -178,7 +182,7 @@ export default function CommentSystem() {
                       href="#"
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
-                      Edit
+                      ویرایش
                     </a>
                   </li>
                   <li>
@@ -186,7 +190,7 @@ export default function CommentSystem() {
                       href="#"
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
-                      Remove
+                      حذف
                     </a>
                   </li>
                   <li>
@@ -194,114 +198,14 @@ export default function CommentSystem() {
                       href="#"
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
-                      Report
+                      گزارش
                     </a>
                   </li>
                 </ul>
               </div>
             </footer>
-            <p className="text-gray-500 dark:text-gray-400">
-              Much appreciated! Glad you liked it ☺️
-            </p>
-            <div className="flex items-center mt-4 space-x-4">
-              <button
-                type="button"
-                className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="w-4 h-4 mr-1"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                  ></path>
-                </svg>
-                Reply
-              </button>
-            </div>
-          </article>
-          <article className="p-6 mb-6 text-base bg-white border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-            <footer className="flex items-center justify-between mb-2">
-              <div className="flex items-center">
-                <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                  <img
-                    className="w-6 h-6 mr-2 rounded-full"
-                    src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
-                    alt="Bonnie Green"
-                  />
-                  Bonnie Green
-                </p>
-                <p className="text-sm text-gray-600 dark:text-gray-400">
-                  <time dateTime="2022-03-12" title="March 12th, 2022">
-                    Mar. 12, 2022
-                  </time>
-                </p>
-              </div>
-              <button
-                id="dropdownComment3Button"
-                data-dropdown-toggle="dropdownComment3"
-                className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                type="button"
-              >
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
-                </svg>
-                <span className="sr-only">Comment settings</span>
-              </button>
 
-              <div
-                id="dropdownComment3"
-                className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-36 dark:bg-gray-700 dark:divide-gray-600"
-              >
-                <ul
-                  className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="dropdownMenuIconHorizontalButton"
-                >
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Edit
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Remove
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Report
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </footer>
-            <p className="text-gray-500 dark:text-gray-400">
-              The article covers the essentials, challenges, myths and stages
-              the UX designer should consider while creating the design
-              strategy.
-            </p>
+            <p className="text-gray-500 dark:text-gray-400">کامنت من این است</p>
             <div className="flex items-center mt-4 space-x-4">
               <button
                 type="button"
@@ -316,13 +220,13 @@ export default function CommentSystem() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                   ></path>
                 </svg>
-                Reply
+                پاسخ
               </button>
             </div>
           </article>
