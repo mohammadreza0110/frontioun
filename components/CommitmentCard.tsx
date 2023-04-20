e3521d6eca78a5837d316273a336239893c9f968
Mhmdrza <mohammadreza0216@gmail.com>
2023-04-21 00:56:44 +0330

    fix: remove hover class

--- components/CommitmentCard.tsx
+++ components/CommitmentCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function GarrantyCard({ children, title }: Props) {
   return (
-    <div className="flex flex-col items-center justify-center w-40 h-40 transition-all duration-200 hover:scale-105 hover:commitment_card bg-slate-200 hover:bg-white rounded-3xl">
+    <div className="flex flex-col items-center justify-center w-40 h-40 transition-all duration-200 bg-white rounded-3xl">
       {children}
       <span>{title}</span>
     </div>
