714e8666233d4517a26448ebb8b90da26152ff0f
Mhmdrza <mohammadreza0216@gmail.com>
2023-05-06 12:13:31 +0330

    fix: configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["flowbite.com", "example.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
